e893c4ae07fb6b59060e69f00e06e33de988ee57
arthurariza <tucaariza@gmail.com>
2023-09-02 17:43:49 -0300

    FEAT: assign character

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
 class Player < ApplicationRecord
+  CHARACTERS = %w[X O].freeze
+
   belongs_to :tic_tac_toe
+
+  validates :name, presence: true
+
+  before_create :set_character
+
+  private
+
+  def set_character
+    self.character = (Player::CHARACTERS - tic_tac_toe.players.pluck(:character)).sample
+  end
 end
